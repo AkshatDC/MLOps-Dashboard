cc6fbdcc2324132d014438c23d3839393b7e4841
Ishita Akolkar <152607634+ishitaakolkar@users.noreply.github.com>
2025-08-07 17:11:38 +0530

    Update

---
+++ components/ModelMetricsDashboard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Register Chart.js components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
